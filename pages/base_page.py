16984184f23839d3425853faa17b3b247187795f
Trokin Roman <rtrokin@gmail.com>
2022-09-21 22:26:27 +0300

    added check for element existence

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,14 +1,23 @@
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 
 class BasePage():
 
-    def __init__(self, driver, url):
+    def __init__(self, driver, url, implicitly_wait=1):
         self.driver = driver
         self.url = url
+        self.driver.implicitly_wait(implicitly_wait)
+
+    def find_elem(self, css_selector):
+        return self.driver.find_element(By.CSS_SELECTOR, f'{css_selector}')
 
     def open(self):
         self.driver.get(self.url)
 
-    def find_item(self, css_selector):
-        return self.driver.find_element(By.CSS_SELECTOR, f'{css_selector}')
+    def is_element_present(self, css_selector):
+        try:
+            self.find_elem(css_selector)
+        except NoSuchElementException:
+            return False
+        return True
--- pages/main_page.py
+++ pages/main_page.py
@@ -4,5 +4,9 @@ from .base_page import BasePage
 class MainPage(BasePage):
 
     def go_to_login_page(self):
-        login_link = self.find_item('#login_link')
+        login_link = self.find_elem('#login_link')
         login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(
+            '#login_link'), 'Login link is not presented'
